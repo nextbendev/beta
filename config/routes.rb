61720fcdef5ea0a749d4c80ee0b24011a3898528
nextbendev <gastronomicllc@gmail.com>
2019-03-14 15:10:59 -0400

    blog and comments setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  resources :articles do
+    resources :comments
+  end
+
+
+  root "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

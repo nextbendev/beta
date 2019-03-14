be64ee0936bd28dff87a8e89114e4e7477b47c1c
nextbendev <gastronomicllc@gmail.com>
2019-03-14 15:20:59 -0400

    blog and comment final

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,20 @@
 
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   http_basic_authenticate_with name: "dhh", password: "secret",
   except: [:index, :show]
 
+  def set_article
+     @article = Article.find(params[:id])
+  end
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def new
@@ -18,7 +22,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -32,7 +36,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+
 
     if @article.update(article_params)
       redirect_to @article
@@ -42,7 +46,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
 
     redirect_to articles_path

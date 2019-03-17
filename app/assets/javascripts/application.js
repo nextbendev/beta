1b323129d6e045ad149c4d3738f183232a2306fc
nextbendev <gastronomicllc@gmail.com>
2019-03-17 12:28:38 -0400

    links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$('.menu-bar').on('click', function(){
+	$('nav ul').toggleClass('reveal');
+});
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
   get 'home/index'
+  get '/articles', to: 'articles#index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
